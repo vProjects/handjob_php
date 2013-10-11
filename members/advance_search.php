fd1e0a2a94e648d9cd1a5761a5ca42e5c1c990d6
vProjects <github@vyrazu.com>
2013-10-11 20:03:17 +0530

    dipanjan
    advace search page is done....

---
+++ members/advance_search.php
@@ -0,0 +1,228 @@
+<?php
+	$page_title = 'ADV_SEARCH';
+	//get header
+	include ('v-templates/header.php');
+
+	//get the horizontal navbar
+	include ('v-templates/navbar.php');
+?>
+
+	<div id="bodyContainer" class="row-fluid advaced_search_form">
+    	<div class="span8 offset2">
+    	<form action="" class="form-horizontal" method="post">
+            <div class="row-fluid">
+                <div class="span10 offset2 form_part">
+                	<h5>Find photo sets or movies that contain: </h5>
+                     <div class="control-group">
+                        <label class="control-label">All these words</label>
+                        <div class="controls">
+                        <input type="text" placeholder="">
+                        </div>
+                     </div>
+                      <div class="control-group">
+                        <label class="control-label">Any of these words</label>
+                        <div class="controls">
+                        <input type="text" placeholder="">
+                        </div>
+                     </div>
+                     <div class="control-group">
+                        <label class="control-label">This exact word or phrase</label>
+                        <div class="controls">
+                        <input type="text" placeholder="">
+                        </div>
+                     </div>
+                </div>
+            </div>
+            
+            <div class="row-fluid">
+                <div class="span10 offset2 form_part">
+                	<h5>But don't show results that have: </h5>
+                      <div class="control-group">
+                        <label class="control-label">All these words</label>
+                        <div class="controls">
+                        <input type="text" placeholder="">
+                        </div>
+                     </div>
+                </div>
+            </div>
+            
+            <div class="row-fluid">
+                <div class="span10 offset2 form_part"> 
+                	<h5>Only show results for: </h5>
+                      <div class="control-group">
+                        <label class="control-label">Content added within</label>
+                        <div class="controls">
+                        <select>
+                            <option>Anytime</option>
+                            <option>3days</option>
+                            <option>7days</option>
+                            <option>14days</option>
+                            <option>30days</option>
+                            <option>60days</option>
+                            <option>90days</option>
+                        </select>
+                        </div>
+                     </div>
+                     <div class="control-group">
+                        <label class="control-label">That contains media types</label>
+                        <div class="controls">
+                        <select>
+                            <option>All Content Type</option>
+                            <option>Movies</option>
+                            <option>Photos</option>
+                            <option>Updates</option>
+                        </select>
+                        </div>
+                     </div>
+                 </div>
+            </div>
+            
+            <div class="row-fluid">
+                <div class="span10 offset2 form_part"> 
+                	 <h5>Sites</h5>
+                      <div class="control-group">
+                     <label class="checkbox inline">
+                        <input type="checkbox" value=""> All Access Pass Main
+                      </label>
+                      <label class="checkbox inline">
+                        <input type="checkbox" value=""> Big Boob Overload	
+                      </label>
+                      <label class="checkbox inline">
+                        <input type="checkbox" value=""> Onion Booty Bangers
+                     </label>
+                     </div>
+                      <div class="control-group">
+                     <label class="checkbox inline">
+                        <input type="checkbox" value=""> Spicy Latinas
+                     </label>
+                     <label class="checkbox inline">
+                        <input type="checkbox" value=""> Sweet & Petite
+                      </label>
+                      </div>
+                      
+                </div>
+            </div> 
+            
+            <div class="row-fluid">
+                <div class="span10 offset2 form_part"> 
+                	 <h5>That match category keyword tags:</h5>    
+                     <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> 2 Dicks
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Amateur	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Anal
+                         </label>
+                     </div>
+                   <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Asian
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Big Cock	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Big Tits
+                         </label>
+                     </div>	
+                     <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Blonde
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Blowjob	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Brunette
+                         </label>
+                     </div>	 	
+                     <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Creampie
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> DP	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Ebony
+                         </label>
+                     </div>
+                      <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Facial
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Gangbang
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Group Sex
+                         </label>
+                     </div>
+                      <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Handjob
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Interracial	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Latina
+                         </label>
+                     </div>	
+                     <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Lesbian
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Lingerie	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Milf
+                         </label>
+                     </div>
+                      <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Petite
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Redhead	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Shaved
+                         </label>
+                     </div>
+                     <div class="control-group">
+                        <label class="checkbox inline">
+                            <input type="checkbox" value=""> Tattoos
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Teen	
+                          </label>
+                          <label class="checkbox inline">
+                            <input type="checkbox" value=""> Threesome
+                         </label>
+                     </div>	
+                    </div>
+            	</div> 
+                <div class="row-fluid">
+                <div class="span10 offset2 form_part_button"> 
+                      <div class="control-group">
+                      		
+                            <button type="submit" class="btn btn-large btn-danger">Clear Form</button>
+                            
+                            <button type="submit" class="btn btn-large btn-primary">List Result</button>
+                            
+                      </div> 
+                </div>
+            </div>  
+        </form>
+    </div>
+    </div>
+
+<?php
+	//include footer
+	include ('v-templates/footer.php');
+?>
