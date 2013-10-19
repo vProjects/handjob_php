75148e4ed73cd0758456f069710a15b2be968a9d
vProjects <github@vyrazu.com>
2013-10-19 16:54:19 +0530

    dipanjan
    blog page is done and footer modified..

---
+++ members/blog_page.php
@@ -0,0 +1,97 @@
+<?php
+	$page_title = 'BLOGS';
+	//get header
+	include ('v-templates/header.php');
+?>
+
+<?php
+	//get the horizontal navbar
+	include ('v-templates/navbar.php');
+?>
+
+<div id="bodyContainer" class="row-fluid">
+	<!--- blog details starts here ---->
+	<div class="span12 blog_page">
+    	<h4><a href="#" class="blog_heading"> Blog Heading</a></h4>
+        <p class="blog_author_name"> Author Name</p>
+        <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+        stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan 
+        helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, 
+        qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip 
+        quis cardigan american apparel, butcher voluptate nisi qui. Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+        stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan 
+        helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, 
+        qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip 
+        quis cardigan american apparel, butcher voluptate nisi qui.</p>
+        <div class="row-fluid">
+        	<ul class="thumbnails">
+            	<div class="span4">
+                	<div class="thumbnail">
+                    	<img class="lazy" data-src="images/video.jpg" src="" alt="vdeo">
+                    </div>
+                </div>
+                <div class="span4">
+                	<div class="thumbnail">
+                    	<img class="lazy" data-src="images/video.jpg" src="" alt="vdeo">
+                    </div>
+                </div>
+                <div class="span4">
+                	<div class="thumbnail">
+                    	<img class="lazy" data-src="images/video.jpg" src="" alt="vdeo">
+                    </div>
+                </div>
+            </ul>
+        </div>
+        <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+        stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan 
+        helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, 
+        qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip 
+        quis cardigan american apparel, butcher voluptate nisi qui. Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+        stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan 
+        helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, 
+        qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip 
+        quis cardigan american apparel, butcher voluptate nisi qui.</p>
+        <p> Added 2013/09/08</p>
+        <p> <b>3 Comments</b></p>
+        <div class="row-fluid">
+        	<div class="span10 offset1">
+                <div class="row-fluid blog_comment_outline">
+                    <div class="span2">
+                        <p class="blog_comment_name">Full Name:</p>
+                    </div>
+                    <div class="span10">
+                        <p class=" blog_comment_section_p">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+                stumptown aliqua, retro synth master cleanse.Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+                stumptown aliqua, retro synth master cleanse.Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+                stumptown aliqua, retro synth master cleanse.</p>
+                    </div>
+                </div>
+                <div class="row-fluid blog_comment_outline">
+                    <div class="span2">
+                        <p class="blog_comment_name">Full Name:</p>
+                    </div>
+                    <div class="span10">
+                        <p class=" blog_comment_section_p">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+                stumptown aliqua, retro synth master cleanse.</p>
+                    </div>
+                </div>
+                <div class="row-fluid blog_comment_outline">
+                    <div class="span2">
+                        <p class="blog_comment_name">Full Name:</p>
+                    </div>
+                    <div class="span10">
+                        <p class=" blog_comment_section_p">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu 
+                stumptown aliqua, retro synth master cleanse.</p>
+                    </div>
+                </div>
+            </div>    
+        </div>        
+    </div>
+    <!--- blog details ends here ---->
+</div>
+
+
+<?php
+	//include footer
+	include ('v-templates/footer.php');
+?>
