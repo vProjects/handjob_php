ded40293dca192f92ef8214e93df5b27c2914ea8
vProjects <github@vyrazu.com>
2014-01-09 20:40:54 +0530

    dipanjan
    playing movie description design...

--- members/playing_movie.php
+++ members/playing_movie.php
@@ -41,6 +41,14 @@
 			</div>	
        </div>
        
+       <!-- added by dipanjan for description part -->
+       <div class="row-fluid">
+       		<div class="span12">
+            	<h3 class="movie_description_heading">Movie Description</h3>
+                <p class="movie_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>	
+       </div>
+       
        <div class="row-fluid">
        		<div class="span10 offset1 media" style="height:400px;margin-top:10px;">
             <!-- new video player added here -->
