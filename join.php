45fa9a4f601e8990c4de0e2a60de453e9b40764f
vProjects <github@vyrazu.com>
2014-02-18 04:42:27 +0530

    webmaster and join page created
    anand

--- join.php
+++ join.php
@@ -11,38 +11,89 @@
 <!-- site description part starts here --->
 <div class="container">
 	<div class="row-fluid">
+    	<div class="span12">
+        	<p>All Exclusive Hand Job content, High Res Images and HD videos. Real girls from around the world, watch real couples giving Hand jobs...! Downloadable Zip files Updated weekly. Start Browsing some of our episodes and join the HandJobStop.com family today.</p>
+        </div>
+    </div>
+	
+    <div class="row-fluid">
+    	<div class="span12 join_now">
+        	<p class="red_text">EXCLUSIVE HAND JOBS CONTENT. WMV VIDEO. MP4 VIDEO. FLV-STREAMING. FULL HD VIDEOS. HIGH RES IMAGES. CUSTOM ZIP FILES. UPDATES DAILY. NO DOWNLOAD LIMITATIONS. </p>
+        </div>
+    </div>
+    
+    <div class="row-fluid">
+    	<div class="span10 offset1 join_now">
+        	<img src="images/join.jpg"/>
+        </div>
+    </div>
+    
+	<div class="row-fluid">
     	<div class="span8">
-        	<h3 class="site_heading"> JOIN NOW By Credit Card!! Instant Access</h3>
+        	<h3 class="site_heading red_text"> JOIN NOW By Credit Card!! Instant Access</h3>
         </div>
     </div>
     
     <div class="row-fluid">
     	<div class="span6 join_now">
         	    <form class="form-horizontal">
-                	
-                    <div class="control-group">
-                    	<label class="control-label" for="inputEmail">Username</label>
-                    	<div class="controls">
-                    		<input type="text" id="inputEmail" placeholder="Username">
-                    	</div>
+                <legend class="red_text bottom_border_grey">Fill up the form.</legend>
+                <div class="control-group">
+                    <label class="control-label-v" for="inputEmail">Username</label>
+                    <div class="controls-v">
+                        <input type="text" placeholder="Username">
                     </div>
-                    <div class="control-group">
-                    	<label class="control-label" for="inputPassword">Password</label>
-                    	<div class="controls">
-                    		<input type="password" id="inputPassword" placeholder="Password">
-                    	</div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label-v" for="inputPassword">Password</label>
+                    <div class="controls-v">
+                        <input type="password" placeholder="Password">
                     </div>
-                    <div class="control-group">
-                    	<label class="control-label" for="inputEmail">Email</label>
-                    	<div class="controls">
-                    		<input type="text" id="inputEmail" placeholder="Email">
-                    	</div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label-v" for="inputEmail">Email</label>
+                    <div class="controls-v">
+                        <input type="text" placeholder="Email">
                     </div>
-                </form>
+                </div>
         </div>
-        <div class="span6">
-        
+        <div class="span6 join_now">
+        	<div class="control-group">
+            	<legend class="red_text bottom_border_grey">Choose your option.</legend>
+                <div class="controls">
+                    <input type="radio" name="plan" class="join_element" style="margin:-2px 0 0 0;">
+                	$24.99(USD) for 30 days then $24.99(USD) recurring every 30 days
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <input type="radio" name="plan" class="join_element" style="margin:-2px 0 0 0;">
+                	$39.99(USD) for 30 days (non-recurring)
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <input type="radio" name="plan" class="join_element" style="margin:-2px 0 0 0;">
+                	$59.99(USD) for 90 days then $59.99(USD) recurring every 90 days
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <input type="radio" name="plan" class="join_element" style="margin:-2px 0 0 0;">
+                	$99.99(USD) for 180 days (non-recurring)
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="row-fluid">
+    	<div class="control-group">
+            <div class="controls">
+                <input type="submit" class="btn btn-danger btn-large join-now-btn" value="Join Now">
+                <a href="#"><h4>Forgot Password ? Click Here</h4></a>
+                <a href="#"><h4 class="red_text">Already a member ? Click here to Login</h4></a>
+            </div>
+        </div>
+        </form>
     </div>
 </div>
 <!-- site description part ends here --->
---
+++ webmaster.php
@@ -0,0 +1,72 @@
+<?php
+	$page_title = 'Webmaster';
+	//get header
+	include ('v-templates/header.php');
+
+	//get the horizontal navbar
+	include ('v-templates/navbar.php');
+	
+?>
+
+<!-- site description part starts here --->
+<div class="container">
+	<div class="row-fluid">
+    	<div class="span12">
+        	<p>All Exclusive Hand Job content, High Res Images and HD videos. Real girls from around the world, watch real couples giving Hand jobs...! Downloadable Zip files Updated weekly. Start Browsing some of our episodes and join the HandJobStop.com family today.</p>
+        </div>
+    </div>
+	    
+	<div class="row-fluid">
+    	<div class="span8">
+        	<h3 class="site_heading red_text">Webmasters</h3>
+        </div>
+    </div>
+    
+    <div class="row-fluid">
+    	<div class="span12 join_now">
+        	<legend class="red_text bottom_border_grey">Earn 50% of all Signups & Rebills!</legend>
+        	<p>Our webmaster program is ran through CCBILL. Its a 50/50 program, which means you earn 50% of all initial signups and 50% of all rebills for as long as a customer is a member! Click below to signup!</p>
+        </div>
+    </div>
+    
+    <div class="row-fluid">
+    	<div class="span6 join_now">
+        	    <form class="form-horizontal">
+                <legend class="red_text bottom_border_grey">Check your status.</legend>
+                <div class="control-group">
+                    <label class="control-label-v" for="inputEmail">Current Id</label>
+                    <div class="controls-v">
+                        <input type="text" placeholder="Current Id">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label-v" for="inputEmail">Username</label>
+                    <div class="controls-v">
+                        <input type="text" placeholder="Username">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label-v" for="inputPassword">Password</label>
+                    <div class="controls-v">
+                        <input type="password" placeholder="Password">
+                    </div>
+                </div>
+                <input type="submit" class="btn btn-danger btn-large join-now-btn" value="Check">
+                <a href="join.php"><h4 class="join-now-btn">Click here to Create New Account now.</h4></a>
+                </form>
+        </div>
+    </div>
+    
+    <div class="row-fluid">
+    	<div class="span12 join_now">
+        	<p style="text-align:center;">Below is the URL you will send traffic to. Replace the "<span class="red_text">XXXXXX</span>" with the account number CCBILL provides you when you signed up. Please note that this is one continuous URL. Use this linking code on any banner, graphic, or text link you wish.<br/><br/>http://refer.ccbill.com/cgi-bin/clicks.cgi?CA=909492-0013&PA=<span class="red_text">XXXXXX</span>&HTML=http://www.handjobstop.com/tour2/index.php? <br/><br/>Pick what you need to promote. If you need more pictures or video clips for your promo pages please contact me I'll be glad to help you. <a href="mailto:filmme37@yahoo.com">filmme37@yahoo.com</a><br/><br/>ICQ: 176998299
+</p>
+        </div>
+    </div>
+</div>
+<!-- site description part ends here --->
+
+<?php
+	//include footer
+	include ('v-templates/footer.php');
+?> 
