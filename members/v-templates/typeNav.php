03377be1571e1c5538ad891ca237de09b00ec468
vProjects <github@vyrazu.com>
2014-03-07 22:08:58 +0530

    tweaks fixed
    anand

--- members/v-templates/typeNav.php
+++ members/v-templates/typeNav.php
@@ -2,6 +2,6 @@
     <div class="btn-group">
       <a href="<?php echo $pageName ?>?type=recent"><button class="btn <?php if($type=='recent'){echo 'active';} ?> btn-large btn-danger border_radius_l">Most Recent</button></a>
       <a href="<?php echo $pageName ?>?type=rated"><button class="btn <?php if($type=='rated'){echo 'active';} ?> btn-large btn-danger">Most Popular</button></a>
-      <a href="<?php echo $pageName ?>?type=name"><button class="btn <?php if($type=='name'){echo 'active';} ?> btn-large btn-danger border_radius_r">Name/Title</button></a>
+      <a href="<?php echo $pageName ?>?type=name"><button class="btn <?php if($type=='name'){echo 'active';} ?> btn-large btn-danger border_radius_r">A-Z</button></a>
     </div>	
 </div>
